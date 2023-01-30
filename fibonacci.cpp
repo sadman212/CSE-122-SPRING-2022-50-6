ed3f0f8715e10ffe27d4dbfb43a83d134813257e
sadman <122214382+sadman212@users.noreply.github.com>
2023-01-30 09:50:11 +0600

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int fib(int n){
+if(n<=1)
+    return n;
+return fib(n-1)+fib(n-2);
+}
+int main(){
+    int n=9;
+    cout<<fib(n);
+    getch();
+    return 0;
+}
